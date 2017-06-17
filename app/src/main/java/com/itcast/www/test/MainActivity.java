8e549a1a2dcb803cf1ac0ef0dada9ff6b5d4d796
WangHaiWang <2943326044@qq.com>
2017-06-17 16:41:15 +0800

    teacherddddd

---
+++ app/src/main/java/com/itcast/www/test/MainActivity.java
@@ -0,0 +1,48 @@
+package com.itcast.www.test;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.widget.FrameLayout;
+
+import org.xutils.common.Callback;
+import org.xutils.http.RequestParams;
+import org.xutils.view.annotation.ContentView;
+import org.xutils.view.annotation.ViewInject;
+import org.xutils.x;
+
+import javax.crypto.spec.RC5ParameterSpec;
+
+@ContentView(R.layout.activity_main)
+public class MainActivity extends FragmentActivity {
+    @ViewInject(R.id.fl)
+    private FrameLayout fl;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+        x.view().inject(this);
+
+        RequestParams params = new RequestParams("http://www.baidu.com");
+        x.http().get(params, new Callback.CommonCallback<String>() {
+
+        });
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.fl, new HomeFragment());
+        ft.commit();
+//        new Handler().postDelayed()
+        Student s = new Student();
+
+        x.http().get(params, new Callback.CommonCallback<String>() {
+        })
+
+                new AsyncTask<>()
+    }
+
+
+}
